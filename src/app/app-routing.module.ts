0eef48a7abf7ceab239819934b3c4653979be2bd
Muhammad Ehtesham <Shani@192.168.10.9>
2020-05-23 00:06:47 +0500

    createproduct component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CreateProductComponent } from "./create-product/create-product.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "create", component: CreateProductComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
