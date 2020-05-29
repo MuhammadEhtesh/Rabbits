387ff83964f8b7b9412cc92d660a18b414cd3787
Muhammad Ehtesham <Shani@muhammads-air.home>
2020-05-29 13:22:45 +0500

    bootstrap removed from create product and angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
+import { MatInputModule } from "@angular/material/input";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CreateProductComponent } from "./Product/create-product/create-product.component";
-import { ProductsComponent } from './Product/products/products.component';
+import { ProductsComponent } from "./Product/products/products.component";
 
 @NgModule({
   declarations: [AppComponent, CreateProductComponent, ProductsComponent],
   imports: [
     BrowserModule,
+    MatInputModule,
     MatButtonModule,
     AppRoutingModule,
     HttpClientModule,
