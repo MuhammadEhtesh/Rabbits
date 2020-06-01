cd266670da82e7e9db66fbc9c763257cf1cd80a5
MuhammadEhtesh <david1.1parker@gmail.com>
2020-06-01 20:23:00 +0500

    navbar component & search bar added

--- backend/models/masterCategory.js
+++ backend/models/masterCategory.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const masterCategorySchema = mongoose.Schema(
+const masterCategorySchema = new Schema(
   {
     name: { type: String, required: true },
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { MatInputModule } from "@angular/material/input";
 import { BrowserModule } from "@angular/platform-browser";
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatIconModule } from "@angular/material/icon";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { NavbarComponent } from "./Components/navbar/navbar.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ProductsComponent } from "./Components/Product/products/products.component";
 import { CreateProductComponent } from "./Components/Product/create-product/create-product.component";
@@ -17,17 +20,20 @@ import { CreateMasterCategoryComponent } from "./Components/Category/create-mast
 @NgModule({
   declarations: [
     AppComponent,
-    CreateProductComponent,
+    NavbarComponent,
     ProductsComponent,
+    CreateProductComponent,
     CreateMasterCategoryComponent,
   ],
   imports: [
     BrowserModule,
     MatCardModule,
+    MatIconModule,
     MatInputModule,
     MatButtonModule,
     AppRoutingModule,
     HttpClientModule,
+    MatToolbarModule,
     MatPaginatorModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
